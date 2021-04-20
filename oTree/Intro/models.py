d79f770b4710deacc6c20ecf226d7c63535fb9e5
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2021-04-20 10:37:16 +0200

    count  wrong answers

--- oTree/Intro/models.py
+++ oTree/Intro/models.py
@@ -45,31 +45,44 @@ class Player(BasePlayer):
     window_height = models.IntegerField(doc="Documents the respondent's browser window's height.")
     browser = models.StringField(doc="Documents the respondent's browser (incl. its version).")
 
+    # Wrong Answers
+    wrong_answer1_1 = models.IntegerField(doc="Counts the number of wrong guesses for CQ1_1.", initial=0)
+    wrong_answer1_2 = models.IntegerField(doc="Counts the number of wrong guesses for CQ1_2.", initial=0)
+    wrong_answer2_1 = models.IntegerField(doc="Counts the number of wrong guesses for CQ2_1.", initial=0)
+    wrong_answer2_2 = models.IntegerField(doc="Counts the number of wrong guesses for CQ2_2.", initial=0)
+    wrong_answer3_1 = models.IntegerField(doc="Counts the number of wrong guesses for CQ3_1.", initial=0)
+    wrong_answer3_2 = models.IntegerField(doc="Counts the number of wrong guesses for CQ3_2.", initial=0)
+
 
 
     CQ1_1 = models.IntegerField(doc="Comprehension Question 1.1", initial=int(Constants.initial_endowment), blank = True)
     def CQ1_1_error_message(self, value):
         if value != int(Constants.initial_endowment):
+            self.wrong_answer1_1 += 1
             return "Leider falsch."
 
     CQ1_2 = models.IntegerField(doc="Comprehension Question 1.2", initial=int(Constants.initial_endowment), blank = True)
     def CQ1_2_error_message(self, value):
         if value != int(Constants.initial_endowment):
+            self.wrong_answer1_2 += 1
             return "Leider falsch."
 
     CQ2_1 = models.IntegerField(doc="Comprehension Question 2.1", initial=int(Constants.initial_endowment * Constants.efficiency_factor), blank = True)
     def CQ2_1_error_message(self, value):
         if value != int(Constants.initial_endowment * Constants.efficiency_factor):
+            self.wrong_answer2_1 += 1
             return "Leider falsch."
 
     CQ2_2 = models.IntegerField(doc="Comprehension Question 2.2", initial=int(Constants.initial_endowment * Constants.efficiency_factor), blank = True)
     def CQ2_2_error_message(self, value):
         if value != int(Constants.initial_endowment * Constants.efficiency_factor):
+            self.wrong_answer2_2 += 1
             return "Leider falsch."
 
     CQ3_1 = models.IntegerField(doc="Comprehension Question 3.1", initial=int(30 + Constants.efficiency_factor * 40 / Constants.group_size), blank=True)
     def CQ3_1_error_message(self, value):
         if value != int(30 + Constants.efficiency_factor * 40 / Constants.group_size):
+            self.wrong_answer3_1 += 1
             return "Leider falsch."
 
     CQ3_2 = models.BooleanField(doc="Comprehension Question 3.2",
@@ -80,6 +93,10 @@ class Player(BasePlayer):
                                     [False, "höher"]
                                 ]
                                 )
+    def CQ3_2_error_message(self, value):
+        if not value:
+            self.wrong_answer3_2 += 1
+            return "Leider falsch."
 
     # CQ3_2 = models.IntegerField(doc="Comprehension Question 3.2", initial=int(30 - 16 + Constants.efficiency_factor * (40+16) / Constants.group_size), blank=True)
     # def CQ3_2_error_message(self, value):
