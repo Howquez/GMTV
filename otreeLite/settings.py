4f75b98a19bdfbd5b05ce9cf6879070c6120d962
Hauke Roggenkamp <hauke2roggenkamp@gmail.com>
2023-06-12 09:06:35 +0200

    June2023 State

--- otreeLite/settings.py
+++ otreeLite/settings.py
@@ -36,7 +36,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.10,
+    real_world_currency_per_point=0.02,
     participation_fee=2.00,
     doc=""
 )
